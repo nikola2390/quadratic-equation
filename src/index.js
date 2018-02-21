6a90b126ec1a5ab662dfefc68b60a305cbdb2a1e
nikola2390 <nikolas_23@tut.by>
2018-02-21 23:38:11 +0300

    All done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  // ax^2 + bx + c
+
+  let a = equation.slice(0,equation.indexOf('*'));
+  let b = equation.slice(equation.indexOf('*')+5,equation.lastIndexOf('*')).trim();
+
+  if (b[0] == '-') {
+    b = +b.slice(2)*(-1);
+  } else {
+    b = +b.slice(2);
+  }
+
+  let c = equation.slice(equation.lastIndexOf('*') + 4);
+
+  if (c[0] == '-') {
+    c = +c.slice(2)*(-1);
+  } else {
+    c = +c.slice(2);
+  }
+
+  let d = b * b - 4 * a * c;
+
+  let x1 = Math.round(((-1) * b + Math.sqrt(d))/ (2 * a));
+  let x2 = Math.round(((-1) * b - Math.sqrt(d))/ (2 * a));
+
+  return [x1,x2].sort( function(a, b) {return a - b})
 }
